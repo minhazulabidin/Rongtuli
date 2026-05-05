41b671c82897e77e9b4d27d7304852873946b088
minhazulabidin <minhajulabedin648@gmail.com>
2026-05-05 15:37:45 +0600

    font family added

---
+++ client/src/app/(root)/page.js
@@ -0,0 +1,15 @@
+
+import Container from '@/components/commonCompo/Container'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+    <Container>
+        
+    </Container>
+    </div>
+  )
+}
+
+export default page
