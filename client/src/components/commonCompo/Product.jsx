794d3d5f279684a07383437faf087f9bee7fd9df
minhazulabidin <minhajulabedin648@gmail.com>
2026-05-13 12:37:24 +0600

    product responsive edit

--- client/src/components/commonCompo/Product.jsx
+++ client/src/components/commonCompo/Product.jsx
@@ -11,7 +11,7 @@ const Product = ({ item }) => {
   const [activeLHeart, setActiveLHeart] = useState(false);
 
   return (
-    <div className="rounded-[10px] border border-[#E9E9E9] p-2.5">
+    <div className="rounded-[10px] border border-[#E9E9E9] p-2.5 flex flex-col justify-between">
       <div className="relative">
         <Image
           src={item?.thumbnail}
@@ -23,13 +23,12 @@ const Product = ({ item }) => {
 
         <FaHeart
           onClick={() => setActiveLHeart(!activeLHeart)}
-          className={`absolute top-3 right-3.5 cursor-pointer text-xl text-[#99999966] ${
-            activeLHeart ? 'text-[#FF5555]' : ''
-          }`}
+          className={`absolute top-3 right-3.5 cursor-pointer md:text-xl text-base text-[#99999966] ${activeLHeart ? 'text-[#FF5555]' : ''
+            }`}
         />
       </div>
 
-      <div className="mt-4.5 space-y-2.5 px-3.25">
+      <div className="mt-4.5 px-3.25 flex flex-col flex-1 justify-between">
         <div className="font-poppins flex items-center gap-1.25 text-xs">
           <ReactStars
             count={5}
@@ -43,19 +42,18 @@ const Product = ({ item }) => {
           <span>({item?.rating})</span>
         </div>
 
-        {/* TITLE LINK */}
         <Link href={`/${item?.category}/${item?.id}`}>
-          <h2 className="font-poppins cursor-pointer text-base text-[#383838] hover:text-orange-500 md:text-lg">
+          <h2 className="font-poppins cursor-pointer md:text-base text-xm text-[#383838] hover:text-orange-500 md:text-lg duration-300 mb-2.5">
             {item?.title}
           </h2>
         </Link>
 
         <div className="text-orangec flex items-center justify-between">
-          <p className="font-poppins text-sm font-medium md:text-xl">
+          <p className="font-poppins md:text-sm text-xm font-medium md:text-xl">
             {item?.price} TK
           </p>
 
-          <BsBasket size={24} />
+          <BsBasket className='md:text-[24px] text-base' />
         </div>
       </div>
     </div>
