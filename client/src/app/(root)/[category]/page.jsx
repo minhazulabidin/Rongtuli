0d042608ac66a6d2409bcc9362f56402dfd39967
minhazulabidin <minhajulabedin648@gmail.com>
2026-05-14 12:19:35 +0600

    react pagination install

--- client/src/app/(root)/[category]/page.jsx
+++ client/src/app/(root)/[category]/page.jsx
@@ -1,8 +1,23 @@
+import Container from '@/components/commonCompo/Container'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>Category page</div>
+    <main>
+      <Container className="flex justify-between gap-13.25">
+        <aside className='w-75'>
+
+        </aside>
+        <section className='flex-1'>
+          <div>
+            sort and show product
+          </div>
+          <div>
+            
+          </div>
+        </section>
+      </Container>
+    </main>
   )
 }
 
