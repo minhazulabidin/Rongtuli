4116fb670841bd889c6043d345ae3cf68da3121a
introtoxicXXl <minhajulabedin648@gmail.com>
2026-05-15 22:59:14 +0600

    shop side bar done

--- client/src/components/layout/ShopPage/ShopSidebar.jsx
+++ client/src/components/layout/ShopPage/ShopSidebar.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import { Separator } from "@/components/ui/separator";
 import { Slider } from "@/components/ui/slider";
 import { ChevronDown } from "lucide-react";
@@ -24,91 +25,80 @@ const ShopSidebar = () => {
         <section className="mt-10.75 font-poppins">
 
             {/* Categories */}
-            <div className="">
-                <h3 className="text-[20px] font-semibold text-[#333] mb-3">
-                    Related Categories
-                </h3>
-
-                <ul className="space-y-2">
-                    {categories.map((category, index) => (
-                        <li
-                            key={index}
-                            onClick={() => setSelectedCategory(category)}
-                            className={`cursor-pointer text-[17px] transition-all duration-200 ${selectedCategory === category
-                                ? "text-[#0095ff] font-medium"
-                                : "text-[#666]"
-                                }`}
-                        >
-                            • {category}
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            <Accordion>
+                <AccordionItem value="item-1">
+                    <AccordionTrigger className="text-lg font-medium text-[#3D3D3F]"> Related Categories</AccordionTrigger>
+                    <AccordionContent className="space-y-2 pb-0">
+                        {categories.map((category, index) => (
+                            <li
+                                key={index}
+                                onClick={() => setSelectedCategory(category)}
+                                className={`cursor-pointer text-[17px] transition-all duration-200 ${selectedCategory === category
+                                    ? "text-orangec font-medium"
+                                    : "text-[#757575]"
+                                    }`}
+                            >
+                                • {category}
+                            </li>
+                        ))}
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
 
             <Separator className="my-6.25" />
 
             {/* Price Filter */}
-            <div>
-                <div className="flex items-center justify-between">
-                    <h3 className="text-[20px] font-semibold text-[#333]">
-                        Filter by Price
-                    </h3>
+            <Accordion>
+                <AccordionItem value="item-1">
+                    <AccordionTrigger className="text-lg font-medium text-[#3D3D3F]">Filter by Price</AccordionTrigger>
+                    <AccordionContent className="">
+                        <Slider
+                            defaultValue={[0]}
+                            max={2500}
+                            step={1}
+                            className="mx-auto w-full mt-4"
+                        />
 
-                    <ChevronDown size={18} className="text-[#333]" />
-                </div>
+                        <p className="mt-4 text-base text-[#A7A7A7]">
+                            Price: <span className="text-orangec">৳1000 - ৳2500</span>
+                        </p>
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
 
-                <div className="mt-6">
-                    <Slider
-                        defaultValue={[0]}
-                        max={2500}
-                        step={1}
-                        className="mx-auto w-full max-w-xs"
-                    />
-
-                    <p className="mt-4 text-[16px] text-[#666]">
-                        Price: ৳1000 - ৳2500
-                    </p>
-                </div>
-            </div>
-
-            <Separator className="my-6.25"/>
+            <Separator className="my-6.25" />
 
             {/* Rating Filter */}
-            <div className="">
-                <div className="flex items-center justify-between">
-                    <h3 className="text-[20px] font-semibold text-[#333]">
-                        Filter by Rating
-                    </h3>
+            <Accordion>
+                <AccordionItem value="item-1">
+                    <AccordionTrigger className="text-lg font-medium text-[#3D3D3F]">Filter by Rating</AccordionTrigger>
+                    <AccordionContent className="mt-4 space-y-3">
+                        {ratings.map((rating) => (
+                            <label
+                                key={rating}
+                                className="flex items-center gap-3 cursor-pointer"
+                            >
+                                <input
+                                    type="checkbox"
+                                    checked={selectedRating === rating}
+                                    onChange={() => setSelectedRating(rating)}
+                                    className="w-4 h-4 cursor-pointer accent-[#0095ff]"
+                                />
 
-                    <ChevronDown size={18} className="text-[#333]" />
-                </div>
-
-                <div className="mt-5 space-y-3">
-                    {ratings.map((rating) => (
-                        <label
-                            key={rating}
-                            className="flex items-center gap-3 cursor-pointer"
-                        >
-                            <input
-                                type="checkbox"
-                                checked={selectedRating === rating}
-                                onChange={() => setSelectedRating(rating)}
-                                className="w-4 h-4 cursor-pointer accent-[#0095ff]"
-                            />
-
-                            <div className="flex items-center gap-1">
-                                {Array.from({ length: rating }).map((_, index) => (
-                                    <FaStar
-                                        key={index}
-                                        className="text-[#f5a623]"
-                                        size={16}
-                                    />
-                                ))}
-                            </div>
-                        </label>
-                    ))}
-                </div>
-            </div>
+                                <div className="flex items-center gap-1">
+                                    {Array.from({ length: rating }).map((_, index) => (
+                                        <FaStar
+                                            key={index}
+                                            className="text-[#f5a623]"
+                                            size={16}
+                                        />
+                                    ))}
+                                </div>
+                            </label>
+                        ))}
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
         </section>
     );
 };
